1c26ccd9514c3e16acf0122c406f640ed00665f3
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-10-06 18:03:31 -0300

    update

--- sbrc-exp.py
+++ sbrc-exp.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  6 16:16:03 2015
+Created on Tue Oct  6 17:56:48 2015
 
 @author: bruno
 """
 
+import sys
+
+
+def main():
+    motes = {}
+    
+    with open(sys.argv[1], "rb") as f:
+       for line in f.readlines():
+           data = line.split()
+           print data
+
+if __name__ == '__main__':
+    main()
