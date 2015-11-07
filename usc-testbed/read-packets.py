6599c6f89398278d56b635da19bf1ba34af1ee18
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-11-06 21:25:06 -0300

    add read-packet.py

---
+++ usc-testbed/read-packets.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 17:56:48 2015
+
+@author: bruno
+"""
+
+import sys
+import os
+
+RTIMER_SECOND = 32768
+RUNTIME = 30
+
+
+nodes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 
+    18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
+    31, 32, 33, 34, 36, 37, 38, 39, 40, 41, 42, 43, 
+    44, 45, 46, 47,48, 49]
+
+
+def main():
+
+  for node in nodes:
+    with open('rpl-normal/mote{}.txt'.format(node), 'r') as f:
+      lines =  f.readlines()
+
+      MAX = len(lines)
+      
+      if MAX > 0:
+        print lines[MAX-1].split()[4]
+             
+
+
+
+if __name__ == '__main__':
+    main()
--- usc-testbed/read-power.py
+++ usc-testbed/read-power.py
@@ -1,4 +1,4 @@
 with open('rpl-normal/power.txt', 'r') as f:
 	for line in f.readlines():
 		data = line.split()
-		print data[3]
+		print data[9]
