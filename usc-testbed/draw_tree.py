20a95ef2c4286113f24a6648c837322fb592c2e4
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-12-01 18:06:23 -0300

    final code draw-rpl

--- usc-testbed/draw_tree.py
+++ usc-testbed/draw_tree.py
@@ -2,25 +2,48 @@
 import pydot
 import sys 
 
-nodes = [2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 17, 
-    18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
+nodes = [2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 
+    18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 
     31, 32, 33, 34, 36, 37, 38, 39, 40, 41, 42, 43, 
-    44, 45, 46, 47,48, 49]
+    44, 45, 46, 47,48]
 
-motes_ip = ['6ce6'='1', 'edbc'='2', '50f4'='3', '629b'='4',
-            '6658'='5', '619e'='8', 'cf11'='9' 'f361'='10',
-            '6300'='11', '209c'='12', '79b6'='13', '62b9'='14',
-            'f078'='15', '4baa'='18', '76e7'='19', 'cdeb'='20',
-            '81fc'='21', '53b8'='22', '1d33'='23', '4d6a'='24',
-            '7233'='25', '4527'='26', '46a3'='27', 'fcaf'='28',
-            'c006'='31', '44ea'='32', 'e886'='33', '823d'='34',
-            'b6c8'='36', '4d05'='37', 'e61f'='38', '667e'='39',
-            '7f43'='40', '6449'='41', '117a'='42', '5966'='43',
-            '4d35'='44', '6fb1'='45','ea5d'='46', 'f23f'='47',
-            '4960'='48',
-            ]
+motes_ip = {'6ce6':'1', 'edbc':'2', '50f4':'3', '629b':'4',
+            '6658':'5', '619e':'8', 'cf11':'9', 'f361':'10',
+            '6300':'11', '209c':'12', '79b6':'13', '62b9':'14',
+            'f078':'15', '4baa':'18', '76e7':'19', 'cdeb':'20',
+            '81fc':'21', '53b8':'22', '1d33':'23', '4d6a':'24',
+            '7233':'25', '4527':'26', '46a3':'27', 'fcaf':'28',
+            'c006':'31', '44ea':'32', 'e886':'33', '823d':'34',
+            'b6c8':'36', '4d05':'37', 'e61f':'38', '667e':'39',
+            '7f43':'40', '6449':'41', '117a':'42', '5966':'43',
+            '4d35':'44', '6fb1':'45','ea5d':'46', 'f23f':'47',
+            '4960':'48',
+            }
 
-def print_graph(file):
+def print_graph():
+    graph = pydot.Dot(graph_type='graph')
+    
+
+    for node in nodes:
+        with open('normal-rpl-tree/mote{}.txt'.format(node), 'r') as f:
+            lines = f.readlines()
+            max_line = len(lines)
+            v = str(node)
+            u = lines[max_line-1].split(':')[1]
+            u = u.split('\n')[0]
+            u = motes_ip[u]
+        
+            edge = pydot.Edge(u, v)
+            graph.add_edge(edge)
+
+            graph.write_png('rpl_tree.png'.format())
+  
+
+
+
+
+
+def print_graph1(file):
     graph = pydot.Dot(graph_type='graph')
     try:
         f = open(file, "r")
@@ -36,7 +59,6 @@ def print_graph(file):
     except:
         print "{} does not exist".format(file)
 
-
 def get_tree_files():
 
   for node in nodes:
@@ -50,6 +72,7 @@ def get_tree_files():
 
 
 def main():
+    print_graph()
 
 
 if __name__ == '__main__':
