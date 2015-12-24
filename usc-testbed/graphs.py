e9b3a69101cf36c05d3b5a1f0d78dcb8d3e7405c
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-12-23 23:30:15 -0300

    update graphs

--- usc-testbed/graphs.py
+++ usc-testbed/graphs.py
@@ -14,19 +14,18 @@ import matplotlib.pyplot as plt
 
 
 def pdr_graph():
-    n_groups = 4
+    n_groups = 3
     
-    normal = (70.56, 0, 0, 0 )
-    std_normal = (3.64,0,0, 0)
+    normal = (70.56, 0, 0 )
+    std_normal = (3.64,0,0)
     
-    balanced_80 = (0,  48.43, 0, 0)
-    std_balanced_80 = (0, 7.03, 0, 0)
+    balanced_80 = (0,  48.43, 0)
+    std_balanced_80 = (0, 7.03, 0)
     
-    balanced_90 = (0, 0, 27.61, 0 )
-    std_balanced_90 = (0,0,4.77, 0)
+    balanced_90 = (0, 0, 27.61)
+    std_balanced_90 = (0,0,4.77)
     
-    cmst = (0,0,0,75.15)
-    std_cmst = (0,0,0,0.75)
+
     fig, ax = plt.subplots()
     
     index = np.arange(n_groups)
@@ -52,23 +51,17 @@ def pdr_graph():
     
     rects3 = plt.bar(index, balanced_90, bar_width,
                      alpha=opacity,
-                     color='y',
+                     color='r',
                      yerr=std_balanced_90,
                      error_kw=error_config,
                      label='ALABAMO-90')
 
-    rects4 = plt.bar(index, cmst, bar_width,
-                     alpha=opacity,
-                     color='r',
-                     yerr=std_cmst,
-                     error_kw=error_config,
-                     label='CMST')
     
     
 
     plt.ylabel('Network Delivery Ratio(%)')
     #plt.xticks(index + bar_width, ('MHROF', 'CMST','Balanced 80', 'Balanced 90'))
-    plt.xticks(index + 0.2, ('MHROF','ALABAMO-80', 'ALABAMO-90', 'CMST'))
+    plt.xticks(index + 0.2, ('MHROF','ALABAMO-80', 'ALABAMO-90'))
     #plt.legend(bbox_to_anchor=(1.4, 1))
     
     plt.tight_layout()
@@ -76,20 +69,19 @@ def pdr_graph():
 
 def lifetime_graph():
     
-    great_value = 15.53
-    n_groups = 4
+    great_value = 34.72
+    n_groups = 3
     
-    normal = (great_value/15.53, 0, 0, 0 )
-    std_normal = (0,0,0, 0)
+    normal = (great_value/34.72, 0, 0)
+    std_normal = (0,0,0)
     
-    balanced_80 = (0, great_value/7.71, 0, 0)
-    std_balanced_80 = (0, 0, 0, 0)
+    balanced_80 = (0, great_value/16.69, 0)
+    std_balanced_80 = (0, 0, 0)
     
-    balanced_90 = (0, 0, great_value/9.58, 0)
-    std_balanced_90 = (0,0, 0, 0)
-    
-    cmst = (0, 0,0,great_value/9.72)
-    std_cmst = (0, 0, 0 ,0)
+    balanced_90 = (0, 0, great_value/21.21)
+    std_balanced_90 = (0,0, 0)
+  
+  
     fig, ax = plt.subplots()
     
     index = np.arange(n_groups)
@@ -116,26 +108,34 @@ def lifetime_graph():
     
     rects3 = plt.bar(index, balanced_90, bar_width,
                      alpha=opacity,
-                     color='y',
+                     color='r',
                      yerr=std_balanced_90,
                      error_kw=error_config,
                      label='ALABAMO-90')
-    
-    rects4 = plt.bar(index, cmst, bar_width,
-                     alpha=opacity,
-                     color='r',
-                     yerr=std_cmst,
-                     error_kw=error_config,
-                     label='CMST')   
-    
+
     
     
     plt.ylabel('Normalized Average Lifetime')
-    plt.xticks(index + 0.2, ('MHROF','ALABAMO-80', 'ALABAMO-90', 'CMST'))
+    plt.xticks(index + 0.2, ('MHROF','ALABAMO-80', 'ALABAMO-90'))
     #plt.legend(bbox_to_anchor=(1.4, 1))
     plt.tight_layout()
    
     plt.savefig('lifetime_bar.eps', format='eps')
 
-pdr_graph()
-lifetime_graph()
+
+def lin_graph():
+    x = [27.61, 48.43,70.56]
+    y = [1.62,2.01,1] # 10, not 9, so the fit isn't perfect
+    
+    fit = np.polyfit(x,y,1)
+    fit_fn = np.poly1d(fit) 
+    # fit_fn is now a function which takes in x and returns an estimate for y
+    
+    plt.plot(x,y,'ro',x, fit_fn(x), '--k')
+    plt.xlim(0, 90)
+    plt.ylim(0, 3)
+   
+#pdr_graph()
+#lifetime_graph()
+
+lin_graph()
